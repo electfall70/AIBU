003033c20cd6dc3d1e5f5c43b52c781ad195dbc0
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2021-09-21 16:22:21 +0200

    the cloud generator script works

--- AIBU/Assets/Scripts/EffectsGeneration/CloudGeneration.cs
+++ AIBU/Assets/Scripts/EffectsGeneration/CloudGeneration.cs
@@ -4,30 +4,21 @@ using UnityEngine;
 
 public class CloudGeneration : MonoBehaviour
 {
-    public int horizonatalStackSize = 20;
-    public float cloudHeight;
-    public Mesh quadMesh;
-    public Material cloudMaterial;
-    float offset;
+    public float spawnHeight;
+    public float heightVariation;
+    public int spawnArea;
 
-    public int layer;
-    public Camera camera;
-    private Matrix4x4 matrix;
+    public int cloudAmount;
+    public GameObject cloudGameObject;
 
-    // Update is called once per frame
-    private void Update()
+    private void Start()
     {
-
-        offset = cloudHeight / horizonatalStackSize / 2f;
-        Vector3 startPosition = transform.position + (Vector3.up * (offset * horizonatalStackSize / 2f));
-
-        Debug.Log("in CloudGeneration, update");
-
-        for (int i = 0; i < horizonatalStackSize; i++) 
+        for (int i = 0; i < cloudAmount; i++) 
         {
-            //Debug.Log("in CloudGeneration, updates for-loop");
-            matrix = Matrix4x4.TRS(startPosition - (Vector3.up * offset * i), transform.rotation, transform.localScale);
-            Graphics.DrawMesh(quadMesh, matrix, cloudMaterial, layer, camera, 0, null, true, false, false);
+            Vector3 spawnPosition = new Vector3(Random.Range(transform.position.x - spawnArea, transform.position.x + spawnArea),
+                Random.Range(-spawnHeight, spawnHeight), 
+                Random.Range(transform.position.z - spawnArea, transform.position.z + spawnArea));
+            Instantiate(cloudGameObject, spawnPosition, Quaternion.identity, this.transform);
         }
         
     }
