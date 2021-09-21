5ace27951bbf3f7be3094fbcf6efe252794f596a
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2021-09-21 11:29:33 +0200

    made an options menu
    it can control the audio mixer. and change from fullscreen to windowed.

---
+++ AIBU/Assets/Scripts/MenuScripts/WindowedscreenHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowedscreenHandler : MonoBehaviour
+{
+    private bool inFullscreen;
+    public Image checkBoxImage;
+
+    public void Start()
+    {
+        //check if the game starts in fullscreen
+        if (Screen.fullScreen == true) 
+            {
+            inFullscreen = true;
+            checkBoxImage.enabled = true;
+            }
+    }
+
+    public void WindowedHandler() 
+    {
+
+        if (inFullscreen)
+        { 
+            Debug.Log("set to windowed");
+            Screen.fullScreen = false;
+            inFullscreen = false; 
+            checkBoxImage.enabled = false;
+        }
+        else 
+        {
+            Debug.Log("set to fullscreen");
+            Screen.fullScreen = true;
+            inFullscreen = true;
+            checkBoxImage.enabled = true;
+        }
+    }
+
+}
