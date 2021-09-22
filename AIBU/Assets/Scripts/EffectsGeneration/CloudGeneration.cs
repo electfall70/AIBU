b2b1a0b9def77b9a8e5f313cbc17efd5e6fceb02
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2021-09-22 11:11:59 +0200

    settings on the cloud generators.
    the big clouds have some draw distance problem, I think. idk.

--- AIBU/Assets/Scripts/EffectsGeneration/CloudGeneration.cs
+++ AIBU/Assets/Scripts/EffectsGeneration/CloudGeneration.cs
@@ -6,18 +6,21 @@ public class CloudGeneration : MonoBehaviour
 {
     public float spawnHeight;
     public float heightVariation;
-    public int spawnArea;
+    public int spawnAreaX;
+    public int spawnAreaZ;
 
     public int cloudAmount;
     public GameObject cloudGameObject;
 
     private void Start()
     {
+        float spawnMinHeigh = spawnHeight - heightVariation;
+
         for (int i = 0; i < cloudAmount; i++) 
         {
-            Vector3 spawnPosition = new Vector3(Random.Range(transform.position.x - spawnArea, transform.position.x + spawnArea),
-                Random.Range(-spawnHeight, spawnHeight), 
-                Random.Range(transform.position.z - spawnArea, transform.position.z + spawnArea));
+            Vector3 spawnPosition = new Vector3(Random.Range(0, transform.position.x + spawnAreaX),
+                Random.Range(spawnMinHeigh, spawnHeight), 
+                Random.Range(0, transform.position.z + spawnAreaZ));
             Instantiate(cloudGameObject, spawnPosition, Quaternion.identity, this.transform);
         }
         
